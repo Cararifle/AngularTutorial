382603ad4823a4f34a08709c7d40823de51d316c
Cararifle <fgrandepla@gmail.com>
2026-04-15 10:50:06 +0200

    feat(tutorial): añadir funcionalidad al boton de eliminar categoria

--- src/app/category/category-list/category-list.ts
+++ src/app/category/category-list/category-list.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { CategoryService } from '../category';
 import { MatDialog } from '@angular/material/dialog';
 import { CategoryEditComponent } from '../category-edit/category-edit';
+import { DialogConfirmationComponent } from '../../core/dialog-confirmation/dialog-confirmation';
 
 @Component({
   selector: 'app-category-list',
@@ -48,6 +49,24 @@ export class CategoryListComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe((result) => {
       this.ngOnInit();
+    });
+  }
+
+  deleteCategory(category: Category) {
+    const dialogRef = this.dialog.open(DialogConfirmationComponent, {
+      data: {
+        title: 'Eliminar categoría',
+        description:
+          'Atención si borra la categoría se perderán sus datos.<br> ¿Desea eliminar la categoría?',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.categoryService.deleteCategory(category.id).subscribe((result) => {
+          this.ngOnInit();
+        });
+      }
     });
   }
 }

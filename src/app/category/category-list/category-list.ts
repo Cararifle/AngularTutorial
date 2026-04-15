03e4d88b523657092aa244954833081bec5b0d17
Cararifle <fgrandepla@gmail.com>
2026-04-15 10:05:33 +0200

    init project

---
+++ src/app/category/category-list/category-list.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Category } from '../model/category';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CategoryService } from '../category';
+import { MatDialog } from '@angular/material/dialog'
+import { CategoryEdit } from '../category-edit/category-edit';
+
+@Component({
+    selector: 'app-category-list',
+    standalone: true,
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatTableModule,
+        CommonModule
+    ],
+    templateUrl: './category-list.html',
+    styleUrl: './category-list.scss'
+})
+export class CategoryListComponent implements OnInit{
+    dataSource = new MatTableDataSource<Category>();
+    displayedColumns: string[] = ['id', 'name', 'action'];
+
+    constructor(
+    private categoryService: CategoryService,
+    public dialog: MatDialog,
+  ) { }
+
+  createCategory() {
+    const dialogRef = this.dialog.open(CategoryEdit, {
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
+    });
+  }
+
+    ngOnInit(): void {
+        this.categoryService.getCategories().subscribe(
+            categories => this.dataSource.data = categories
+        );
+    }
+}

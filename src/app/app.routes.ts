712ea1d53991e7b4f2d46a8428466ceedb1a1605
Cararifle <fgrandepla@gmail.com>
2026-04-16 13:32:59 +0200

    feat(tutorial): crear rama para el tutorial de listado paginado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,8 @@ export const routes: Routes = [
     loadComponent: () =>
       import('../app/category/category-list/category-list').then((m) => m.CategoryListComponent),
   },
+  {
+    path: 'authors',
+    loadComponent: () => import('../app/author/author-list/author-list').then((m) => m.AuthorList),
+  },
 ];

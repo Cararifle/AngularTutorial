abdd0c67420d2b42b7b03cf4e3e401246ccc6d02
Cararifle <fgrandepla@gmail.com>
2026-04-21 12:00:48 +0200

    feat: añadir listado basico de cliente

--- src/app/client/client-list/client-list.ts
+++ src/app/client/client-list/client-list.ts
@@ -1,9 +1,75 @@
-import { Component } from '@angular/core';
+import { Category } from './../../category/model/category';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Client } from '../model/client';
+import { MatDialog } from '@angular/material/dialog';
+import { ClientEdit } from '../client-edit/client-edit';
+import { DialogConfirmationComponent } from '../../core/dialog-confirmation/dialog-confirmation';
+import { ClientService } from '../client';
 
 @Component({
   selector: 'app-client-list',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTableModule, CommonModule],
   templateUrl: './client-list.html',
   styleUrl: './client-list.scss',
 })
-export class ClientList {}
+export class ClientList implements OnInit {
+  dataSource = new MatTableDataSource<Client>();
+  displayedColumns: string[] = ['id', 'name', 'action'];
+
+  constructor(
+    private clientService: ClientService,
+    public dialog: MatDialog,
+  ) {}
+
+  ngOnInit(): void {
+    this.clientService.getClients().subscribe({
+      next: (clients) => (this.dataSource.data = clients),
+      error: (err) => {
+        console.error('Error loading clients:', err);
+        this.dataSource.data = [];
+      },
+    });
+  }
+
+  createClient() {
+    const dialogRef = this.dialog.open(ClientEdit, {
+      data: {},
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.ngOnInit();
+    });
+  }
+
+  editClient(client: Client) {
+    const dialogRef = this.dialog.open(ClientEdit, {
+      data: { client },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.ngOnInit();
+    });
+  }
+
+  deleteClient(client: Client) {
+    const dialogRef = this.dialog.open(DialogConfirmationComponent, {
+      data: {
+        title: 'Eliminar cliente',
+        description:
+          'Atencion, si elimina al cliente se perderan sus datos. <br> ¿Desea eliminar el cliente?',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.clientService.deleteClient(client.id).subscribe((result) => {
+          this.ngOnInit();
+        });
+      }
+    });
+  }
+}

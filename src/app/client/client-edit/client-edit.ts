c5e74baaa014bc60d1737c80cb6ff6d11f0e81c7
Cararifle <fgrandepla@gmail.com>
2026-04-21 12:50:46 +0200

    feat: añadir acciones al listado cliente

--- src/app/client/client-edit/client-edit.ts
+++ src/app/client/client-edit/client-edit.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { ClientService } from './../client';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Client } from '../model/client';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-client-edit',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './client-edit.html',
   styleUrl: './client-edit.scss',
 })
-export class ClientEdit {}
+export class ClientEdit implements OnInit {
+  client: Client;
+
+  constructor(
+    public dialogRef: MatDialogRef<ClientEdit>,
+    @Inject(MAT_DIALOG_DATA) public data: { client: Client },
+    private clientService: ClientService,
+  ) {}
+
+  ngOnInit(): void {
+    this.client = this.data.client ? Object.assign({}, this.data.client) : new Client();
+  }
+
+  onSave() {
+    this.clientService.saveClient(this.client).subscribe(() => {
+      this.dialogRef.close();
+    });
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
+}
